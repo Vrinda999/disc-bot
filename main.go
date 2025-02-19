4ad631a7965a5cc4390d7bab218a4dc64c398ac6
Vrinda <abrolvrinda@gmail.com>
2025-02-20 01:21:17 +0530

    feat: db connection

--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 	"iconic-lines/bot"
 	"iconic-lines/config"
+	"iconic-lines/db"
 )
 
 func main() {
+
 	err := config.ReadConfig()
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	bot.Start()
+	// Initialize MongoDB
+	client := db.ConnectDB()
+	defer client.Disconnect(nil) // Ensure we close the connection
+
+	// Start the Discord bot
+	bot.Start(client)
 
 	<-make(chan struct{})
-	return
+
+	fmt.Println("Bot is running...")
+	select {} // Keeps the bot running
 }
